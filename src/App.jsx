931894f42568e7b4ebf9832b851968f8fedcae91
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-01-16 07:56:33 +0600

    :tada: setup: Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className=" bg-red-500 font-poppins text-3xl font-bold underline ">
+        Hello world!
+      </h1>
+    </>
+  );
+}
