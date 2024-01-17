bff690ae10beac997b5514aa6c2a472b58609c31
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-01-17 09:59:03 +0600

    :sparkles: feat: Edit task feature

--- src/Task/AddTaskModal.jsx
+++ src/Task/AddTaskModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export default function AddTaskModal({ onSave }) {
-  const [task, setTask] = useState({
+export default function AddTaskModal({ onSave, taskToUpdate, onCloseClick }) {
+  const [task, setTask] = useState(taskToUpdate || {
     id: crypto.randomUUID(),
     title: "",
     description: "",
@@ -9,6 +9,8 @@ export default function AddTaskModal({ onSave }) {
     priority: "",
     isFavorite: false,
   });
+
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null))
 
   const handleChange = (evt) => {
     const name = evt.target.name;
@@ -29,7 +31,7 @@ export default function AddTaskModal({ onSave }) {
       <div className="absolute left-0 top-0 z-10 h-full w-full bg-black bg-opacity-70"></div>
       <form className="absolute left-1/2 top-1/4 z-10 mx-auto my-10 w-full max-w-[740px] -translate-x-1/2 rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          {isAdd ? "Add New Task" : "Edit Task"}
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -92,13 +94,19 @@ export default function AddTaskModal({ onSave }) {
           </div>
         </div>
 
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-16 flex justify-between lg:mt-20">
+        <button
+            className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={onCloseClick}
+          >
+            Close
+          </button>
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
             onClick={(e) => {
               e.preventDefault();
-              onSave(task);
+              onSave(task, isAdd);
             }}
           >
             Save
